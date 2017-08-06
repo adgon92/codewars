91588bcf716d32435ac005ba8efb95712a653238
Adam Gontarz <adgon92@gmail.com>
2017-08-07 00:03:59 +0200

    [K01] Solution

---
+++ codewars-java/src/main/java/K01checkingGroups/Groups.java
@@ -0,0 +1,68 @@
+package K01checkingGroups;
+
+import lombok.Value;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.toMap;
+
+class Groups {
+
+    private static final List<Group> KNOWN_GROUPS = new ArrayList<>(3);
+
+    private static final Map<Character, Group> STARTING_CHARACTERS;
+    private static final Map<Character, Group> ENDING_CHARACTERS;
+
+    static {
+        KNOWN_GROUPS.add(new Group('[', ']'));
+        KNOWN_GROUPS.add(new Group('(', ')'));
+        KNOWN_GROUPS.add(new Group('{', '}'));
+
+        STARTING_CHARACTERS = KNOWN_GROUPS.stream()
+                .collect(toMap(
+                        Group::getStart,
+                        Function.identity()
+                ));
+
+        ENDING_CHARACTERS = KNOWN_GROUPS.stream()
+                .collect(toMap(
+                        Group::getEnd,
+                        Function.identity()
+                ));
+    }
+
+    boolean groupCheck(String stringToValidate) {
+        Deque<Group> openedGroups = new ArrayDeque<>();
+
+        for (char ch : stringToValidate.toCharArray()) {
+            if (isStaringCharacter(ch)) {
+                openedGroups.push(STARTING_CHARACTERS.get(ch));
+            } else if (ENDING_CHARACTERS.containsKey(ch)) {
+                try {
+                    Group lastMatchingGroup = openedGroups.pop();
+                    if (lastMatchingGroup.end != ch) {
+                        return false;
+                    }
+                } catch (NoSuchElementException ignored) {
+                    return false;
+                }
+            }
+        }
+
+        return openedGroups.isEmpty();
+    }
+
+    private boolean isStaringCharacter(char ch) {
+        return STARTING_CHARACTERS.containsKey(ch);
+    }
+
+    @Value
+    private static class Group {
+
+        char start;
+        char end;
+
+    }
+
+}
---
+++ codewars-java/src/test/java/K01checkingGroups/GroupsTest.java
@@ -0,0 +1,36 @@
+package K01checkingGroups;
+
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class GroupsTest {
+
+    @Test
+    @Parameters({
+            "(),true",
+            "{},true",
+            "({}),true",
+            "[[]()],true",
+            "[{()}],true",
+            "{(}),false",
+            "([],false",
+            "[]),false"
+    })
+    public void shouldTestStringForCorrectGroupsMatching(String stringToValidate, boolean expectedResult) {
+        // given
+        Groups groups = new Groups();
+
+        // when
+        boolean isValid = groups.groupCheck(stringToValidate);
+
+        // then
+        assertThat(isValid).isEqualTo(expectedResult);
+    }
+
+}
