da822926ab847dc592ff57e69bd8480a00d8a998
Adam Gontarz <adgon92@gmail.com>
2017-08-08 23:31:07 +0200

    [J003] My - even better :)

--- codewars-java/src/main/java/J003DontGiveMeFive/Kata.java
+++ codewars-java/src/main/java/J003DontGiveMeFive/Kata.java
@@ -8,13 +8,13 @@ public class Kata {
 
     public static int dontGiveMeFive(int start, int end) {
         return (int) (long) IntStream.rangeClosed(start, end)
-                .map(number -> number > 0 ? number : -number)
+                .map(Math::abs)
                 .filter(Kata::containsNoFive)
                 .count();
     }
 
     private static boolean containsNoFive(final int number) {
-        for (int x = Math.abs(number); x != 0; x /= 10) {
+        for (int x = number; x != 0; x /= 10) {
             if (x % 10 == DIGIT_TO_FIND) return false;
         }
         return true;
